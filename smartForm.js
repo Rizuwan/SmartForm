723bd2559f0565279adbf6e233acf6f99fa8b115
cis101 <cis101@secs.iub.edu.bd>
2018-02-12 09:31:56 +0600

    sf

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,40 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+  name:'',
+  email:'',
+  html: {Likes:[], disLikes:[]},
+  css: {Likes:[], disLikes:[]},
+  js: {Likes:[], disLikes:[]},
+  strength: {css:'',html:'',js:'', },
+  currentQuestion: '#welcome'
+};
+
+$('#start').click(function(){
+  //console.log("start");
+  $('#welcome').hide();
+  $('#q1').show();
+});
+
+// $('#name').change(function(event){
+//   console.log($('#name').val());
+//   // $('#welcome').hide();
+//   // $('#q1').show();
+// });
+
+
+
+
+$('#q1next').click(function(){
+  //console.log("start");
+  if($('#name').val() && $('#name').val()){
+    userData.name= $('#name').val();
+    userData.email= $('#email').val();
+    $('#q2').show();
+    $('#q1').hide();
+  }
+  else {
+    alert('Enter Value');
+  }
+ });
+
